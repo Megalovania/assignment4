a0c7410aa2cc41a5ebf8efc32dd238025407df8d
Cory Cizauskas <coryciz@gmail.com>
2017-04-15 15:00:31 -0400

    Completed Assignment

--- assignment4.js
+++ assignment4.js
@@ -12,6 +12,54 @@
 // keyup events could be helpful to get value of field as the user types
 
 (function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
+  $(".flexsearch-input").keyup(function(event){
+    
+    var input = $(".flexsearch-input").val();
+    $(".flexsearch-predictions ul").empty();
+    $(".flexsearch-predictions").hide();
+    
+    predictSearch(input);
+  
+  
+  });
+  
+  function predictSearch(input){
+    var url = "http://www.mattbowytz.com/simple_api.json?data=all";
+    var googleurl = "http://www.google.com/search?q=";
+    input = input.toLowerCase();
+    
+    
+    if(input.length != 0){
+      $.ajax({
+        url: url,
+        method: 'GET',  
+        datatype: 'json',
+      }).done(function(data){
+        var count=0;
+        //console.log(result);
+        
+        $.each(data['data'], function(key, value){
+          
+          $.each(data['data'][key], function(index, value){
+          
+            console.log(value);
+            if(value.toLowerCase().match("^"+input) && count<4){
+              var searchurl = googleurl+value;
+              searchurl.replace(" ", "+");
+              $('.flexsearch-predictions ul').append('<li><a href="'+searchurl+'">'+value+'</a></li>');
+              
+              count++;
+              $(".flexsearch-predictions").show();
+            }
+            
+          });
+          
+        });
+
+      });
+      
+    }
+    
+  }
+  
 })();
